288c17bb533fe60d7386b94586769f2fbae222b7
abansybana09 <abansybana322@gmail.com>
2025-10-21 19:35:45 +0700

    Pertama kali upload proyek

---
+++ Praktikum_1/tugasm1.kt
@@ -0,0 +1,16 @@
+fun main() {
+    print("Masukkan nilai siswa: ")
+    val nilai = readLine()!!.toInt()
+
+    val grade = if (nilai in 80..100) {
+        "A"
+    } else if (nilai in 60..79) {
+        "B"
+    } else if (nilai in 50..59) {
+        "C"
+    } else {
+        "D"
+    }
+
+    println("Nilai akhir siswa adalah: $grade")
+}
