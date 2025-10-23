3656e481753812985c04e59e46a5f9074f908067
abansybana09 <abansybana322@gmail.com>
2025-10-23 23:27:00 +0700

    -

--- Praktikum_1/tugasm1.kt
+++ Praktikum_1/tugasm1.kt
@@ -4,7 +4,7 @@ fun main() {
 
     val grade = if (nilai in 80..100) {
         "A"
-    } else if (nilai in 60..79) {
+    } else if (nilai in 60..78) {
         "B"
     } else if (nilai in 50..59) {
         "C"
